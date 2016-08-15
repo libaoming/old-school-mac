46e3fe20176f31b949b77138650de837e340b23b
master <libaoming2@gmail.com>
2016-08-15 12:01:49 +0800

    complete loop exercise

--- old-school-mac/ViewController.swift
+++ old-school-mac/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLbl: UILabel!
+    
+    var phrases = ["booting from floppy...\n",
+                   "reading from disk...\n",
+                   "updating registing...\n",
+                   "......\n",
+                   "..........\n",
+                   ".................\n",
+                   "welcome jessica \n it is nice to see you again "]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        mainLbl.text = ""
+        
+//        for x in 0 ..< phrases.count {
+//            
+//            
+//            mainLbl.text! += phrases[x]
+//            
+//            
+//            
+//        }
+        
+        
+        for phase in phrases {
+            
+            mainLbl.text! += phase
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
